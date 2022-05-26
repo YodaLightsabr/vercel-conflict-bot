392ba80ed46fac15f1e9927a328fa18e37f28529
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-05-25 18:32:06 -0700

    fjgdfhgjk

--- .conflict/build/events/message.js
+++ .conflict/build/events/message.js
@@ -1,5 +1,5 @@
 import { onMessageCreate } from '@conflict/beta/events';
-import Welcome from "../views/welcome.js?n=16535276083178838";
+import Welcome from "../views/welcome.js?n=16535283739977084";
 onMessageCreate(message => {
   if (message.content === "hello") message.channel.send(":wave: Hey there!");
   if (message.content === "welcome") message.channel.view(Welcome());
