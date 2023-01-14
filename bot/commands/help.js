c61b513b07a01820a64413c83abca418f68d7d13
Yoda <yoda@yodacode.xyz>
2023-01-14 03:30:09 -0800

    Await

--- bot/commands/help.js
+++ bot/commands/help.js
@@ -6,7 +6,7 @@ export default new Command({
     description: 'Hello there Vercel!',
     options: [],
     execute: async (command, options, utils) => {
-        command.view(
+        await command.view(
             <message>
                 <Embed color="#ff3333" image={{ url: "https://conflict.js.org/favicon.png" }}>
                     <title>Hello, Vercel!</title>
