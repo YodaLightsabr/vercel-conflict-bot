328a14ee4d1e80b5f0f51935b765e9d7e885ac94
Yoda <yoda@yodacode.xyz>
2023-01-14 02:12:37 -0800

    Bump Conflit versionmy-conflict-app@0.1.1 /Users/ian/code/vercel-conflict-bot └── @conflict/beta@0.9.21

--- .conflict/build/commands/help.js
+++ .conflict/build/commands/help.js
@@ -5,11 +5,16 @@ export default new Command({
   description: 'Hello there Vercel!',
   options: [],
   execute: async (command, options, utils) => {
-    command.view(global.__ConflictViewParser("message", null, global.__ConflictViewParser(Embed, {
+    const view = global.__ConflictViewParser("message", null, global.__ConflictViewParser(Embed, {
       color: "#ff3333",
       image: {
         url: "https://conflict.js.org/favicon.png"
       }
-    }, global.__ConflictViewParser("title", null, "Hello, Vercel!"), global.__ConflictViewParser("description", null, "Welcome to **Conflict**."))));
+    }, global.__ConflictViewParser("title", null, "Hello, Vercel!"), global.__ConflictViewParser("description", null, "Welcome to **Conflict**.")));
+
+    console.log({
+      view
+    });
+    command.view(view);
   }
 });
