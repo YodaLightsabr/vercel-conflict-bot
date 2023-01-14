0787e9b90e0e14c134bcf0ab03350c489a3324d8
Yoda <yoda@yodacode.xyz>
2023-01-14 12:20:33 -0800

    Bump Conflit versionmy-conflict-app@0.1.1 /Users/ian/code/vercel-conflict-bot └── @conflict/beta@0.9.37

--- .conflict/build/events/message.js
+++ .conflict/build/events/message.js
@@ -1,5 +1,5 @@
 import { onMessageCreate } from '@conflict/beta/events';
-import Welcome from "../views/welcome.js?n=16737266907709492";
+import Welcome from "../views/welcome.js?n=16737273191231475";
 onMessageCreate(message => {
   if (message.content === "hello") message.channel.send(":wave: Hey there!");
   if (message.content === "welcome") message.channel.view(Welcome());
